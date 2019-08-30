225db172b9ed8382c3ce8cc0cb792c6fa1dc0a5e
lee <lee-dasilva@hotmail.com>
2019-08-30 16:41:47 +0100

    remove some tests thats not ready to commit

--- js/bool.js
+++ js/bool.js
@@ -1,1 +1,28 @@
 //basicaly is an object wrapper for a boolean value 
+//must not confuse the primitive Boolean with the true and false values of the Boolean object
+//ex:
+
+let objBoolean = new Boolean(false)
+
+if(objBoolean) {
+    console.log("objBoolean is in: ", objBoolean)
+} 
+
+let priBoolean = false
+
+if (priBoolean) {
+    console.log("priBoolean is in: ", prinBoolean)
+}
+
+//a values that's 0, "", null, undefined, NaN are false
+//ex:
+let a
+let b = null
+
+console.log(Boolean(b))
+
+//more Exemples:
+
+let defineFalse = new Boolean(false)
+let trueFalse = Boolean(defineFalse)
+console.log(defineFalse, trueFalse)
