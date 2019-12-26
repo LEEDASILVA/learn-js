7e5dd6082da74007d2109fc3454d15dfa9b39727
lee <lee-dasilva@hotmail.com>
2019-12-26 23:35:03 +0000

    regex expressions

--- regexp.js
+++ regexp.js
@@ -1,5 +1,91 @@
-const string = 'all your base are belong to us'
-const regex = /base/
+const devision = title => {console.log(`\n------------${title}-------------`)}
 
-const isExisting = regex.test(string)
-console.log(isExisting)
+let string = 'all your base are belong to us'
+let regex = /base/
+let faildString = ' all you are belong to us'
+// see if a smaller string exists in a bigger string
+// using `test`
+const isExisting = str => regex.test(str)
+console.log(string, regex, isExisting(string))
+console.log(faildString, regex, isExisting(faildString))
+
+
+devision('^$ sign')
+string = 'baseaaa'
+faildString = '-base-'
+regex = /^base/
+console.log(string, regex, isExisting(string))
+console.log(faildString, regex, isExisting(faildString))
+
+regex = /base$/
+string = 'aaabase'
+console.log(string, regex, isExisting(string))
+console.log(faildString, regex, isExisting(faildString))
+
+string = 'base'
+regex = /^base$/
+console.log(string, regex, isExisting(string))
+console.log(faildString, regex, isExisting(faildString))
+
+devision('[] and + * ?')
+
+string = 'my name is something and my age is 24'
+faildString = 'my name is Smething and my age is 24'
+
+// will find the match in wich the 'name is [a-z]+' is the substring of all the letter form
+// a -> z and the + is that can be one or more matchs of [a-z]
+regex = /name is [a-z]+/
+
+// fuctions that return the match
+// the exec return an array of information from the regex expression
+const match = str => {
+    let arr = regex.exec(str)
+    arr == null ? console.log(arr, 'erro!!') : console.log('result ->', arr[0])
+}
+
+console.log(string, regex, isExisting(string))
+match(string)
+console.log(faildString, regex, isExisting(faildString))
+match(faildString)
+
+faildString = 'my name is somethin and my age is 24'
+faildString2 = 'my name is somethi1 and my age is 24'
+regex = /my name is something?/
+
+console.log(regex)
+match(string)
+match(faildString)
+match(faildString2)
+
+devision('{size}')
+
+regex = /name is [a-z]{1,3}/
+
+console.log(string, regex, isExisting(string))
+match(string)
+
+devision('grouping ()')
+
+string = 'name is something'
+regex = /name is ([a-z]+)/
+const matchName = regex.exec(string)
+
+// the array obtain from the regex expression
+// contains in the position 1 the grouped expression
+console.log(matchName)
+const name = matchName[1]
+console.log(name)
+
+devision('small and more complex example')
+
+string = 'filea.mp3 file_1.mp3 file_2.mp3 hello.csv logs.log test.txt'
+regex = /(\w+)\.mp3/g
+
+let match1 = regex.exec(string)
+
+while (match1) {
+    const files = match1[1]
+    console.log(match1)
+    console.log(files)
+    match1 = regex.exec(string)
+}
--- regexp.md
+++ regexp.md
@@ -11,3 +11,46 @@ let reg = /ab+c/
 - you can use the constructor function `RegExp('regular expression')`.
 
 ## writing a regular expression pattern
+
+- So regex is a way matching patters in a string
+
+- the `^` sign it's used to specify the start of the string has to match the start of the expression
+
+- the `$` sign it's used to assign the end of the `string`
+
+- the `[a-z]` is all the letter from a to z lower case
+
+- the `+` sign is 1 or more, the `*` is 0 or more
+
+- the `?` it's when the rune or the expression is optional
+
+- the `{1,3}` this will give a limit to the expression, let say that it gives the length of the string you what to match
+
+- `\d` is the same of saying `[a-zA-Z0-9_]` so everything from a to z lower and upper case, numbers and underscore
+
+- `\s` matches the spaces
+
+- `\n` matches the newlines
+
+- `\t` matches the tabs
+
+- `\w` matches the works
+
+- `\g` global all the matches in the string
+
+- grouping -> `()` useful for obtaining values of a string for example:
+
+```javascript
+const string = 'name is something'
+const regex = /name is ([a-z]+)/
+
+const match = regex.exec(string)
+
+const name = match[1]
+console.log(name)
+// something
+```
+
+ so the regex will get the match from a string that has one or more letters from a to z, saves it in an array where the first position is the name
+
+## **you can see all this in the regexp.js**
