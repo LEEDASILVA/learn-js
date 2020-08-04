ca63aa60d4da3d7e9e252e4fc16362ddf7642f20
lee <lee-dasilva@hotmail.com>
2020-08-04 11:51:01 +0100

    removing lodash

--- debounce/debounce.js
+++ debounce/debounce.js
@@ -1,55 +1,42 @@
 const _ = require('lodash')
 
-const debounce = (func, wait, options = {}) => {
-  let timeout = 0
-  return function (...args) {
-    let context = this
-    let nothing = !Object.entries(options).length
-      ? false
-      : !options.leading && !options.trailing
-    let firstExec = options.leading || (options.leading && options.trailing)
-    // Should the function be called now? If firstExec is true
-    // and not already in a timeout then the answer is: Yes
-    let callNow = firstExec && !timeout
+// const debounce = (func, wait, options = {}) => {
+//   let timeout = 0
+//   return function (...args) {
+//     let context = this
+//     let nothing = !Object.entries(options).length
+//       ? false
+//       : !options.leading && !options.trailing
+//     let firstExec = options.leading || (options.leading && options.trailing)
+//     // Should the function be called now? If firstExec is true
+//     // and not already in a timeout then the answer is: Yes
+//     let callNow = firstExec && !timeout
 
-    // This is the basic debounce behaviour where you can call this
-    // function several times, but it will only execute once
-    // [before or after imposing a delay].
-    // Each time the returned function is called, the timer starts over.
-    clearTimeout(timeout)
+//     // This is the basic debounce behaviour where you can call this
+//     // function several times, but it will only execute once
+//     // [before or after imposing a delay].
+//     // Each time the returned function is called, the timer starts over.
+//     clearTimeout(timeout)
 
-    // Set the new timeout
-    timeout = setTimeout(function () {
-      // Inside the timeout function, clear the timeout variable
-      // which will let the next execution run when in 'firstExec' mode
-      timeout = 0
+//     // Set the new timeout
+//     timeout = setTimeout(function () {
+//       // Inside the timeout function, clear the timeout variable
+//       // which will let the next execution run when in 'firstExec' mode
+//       timeout = 0
 
-      // Check if the function already ran with the firstExec flag
-      if (!firstExec && !nothing) {
-        // Call the original function with apply
-        // apply lets you define the 'this' object as well as the arguments
-        //    (both captured before setTimeout)
-        func.apply(context, args)
-      }
-    }, wait)
+//       // Check if the function already ran with the firstExec flag
+//       if (!firstExec && !nothing) {
+//         // Call the original function with apply
+//         // apply lets you define the 'this' object as well as the arguments
+//         //    (both captured before setTimeout)
+//         func.apply(context, args)
+//       }
+//     }, wait)
 
-    // Immediate mode and no wait timer? Execute the function..
-    if (callNow && !nothing) func.apply(context, args)
-  }
-}
-
-// const printStr = (str) => console.log(str)
-
-// const run = (callback, callLimit, nbr) =>
-//   new Promise((r) => {
-//     let inter = setInterval(() => callback('merda'), callLimit)
-//     setTimeout(() => {
-//       clearInterval(inter)
-//       r()
-//     }, callLimit * nbr)
-//   })
-
-// run(debounce(printStr, 1000), 1000, 3).then((v) => v)
+//     // Immediate mode and no wait timer? Execute the function..
+//     if (callNow && !nothing) func.apply(context, args)
+//   }
+// }
 
 /*
 the wait takes X ms to execute the callback function
@@ -74,25 +61,38 @@ ex: run(debounce(printStr, 1000), 1000, 5).then((v) => v)
 { trailing: true, leading: true } -> executes on the first moment the function is called
 */
 
-const onMouseMove = (e) => {
-  console.clear()
-  console.log(e)
-  console.log('x: ', e.x, 'y: ', e.y)
-}
+// const onMouseMove = (e) => {
+//   console.clear()
+//   console.log(e)
+//   console.log('x: ', e.x, 'y: ', e.y)
+// }
 
-// Call the debounced function on every mouse move
-window.addEventListener('mousemove', debounce(onMouseMove, 50))
+// // Call the debounced function on every mouse move
+// window.addEventListener('mousemove', debounce(onMouseMove, 50))
 
-// const add = (arr, el) => arr.push(el)
+const add = (arr, el) => arr.push(el)
 
-// const run = (callback, callLimit, nbr) =>
-//   new Promise((r) => {
-//     let arr = []
-//     let inter = setInterval(() => callback(arr, 1), callLimit)
-//     setTimeout(() => {
-//       clearInterval(inter)
-//       r(arr)
-//     }, callLimit * nbr)
-//   })
+const print = (str) => console.log(str)
+const run = (callback, callLimit, nbr) =>
+  new Promise((r) => {
+    let arr = []
+    let inter = setInterval(() => callback(arr, 1), callLimit)
+    setTimeout(() => {
+      clearInterval(inter)
+      r(arr)
+    }, callLimit * nbr)
+  })
 
-// run(_.debounce(add, 600), 500, 5).then((v) => console.log(v))
+// run(_.debounce(print, 600, { leading: false, trailing: false }), 1000, 5).then((v) => console.log(v))
+// run(_.debounce(print, 600, { leading: true, trailing: false }), 5000, 5).then((v) => console.log(v))
+// run(_.debounce(print, 600, { leading: false, trailing: true }), 5000, 5).then((v) => console.log(v))
+// run(_.debounce(print, 600, { leading: true, trailing: true }), 5000, 5).then((v) => console.log(v))
+// run(_.debounce(print, 600), 1000, 5).then((v) => console.log(v))
+// run(_.debounce(print, 5000, { leading: true, trailing: false }), 600, 5).then((v) => console.log(v))
+
+// // executes last thing so pushing somrthing into the array permites us to now if it was executed at last
+// run(_.debounce(add, 5000, { leading: false, trailing: true }), 2000, 5).then((v) => console.log(v))
+// executes on the first time, same here
+run(_.debounce(add, 5000, { leading: true, trailing: false }), 2000, 5).then((v) => console.log(v))
+// // executes last thing
+// run(_.debounce(add, 50, { leading: true, trailing: true }), 20, 5).then((v) => console.log(v))
--- jsexercism/space-age/space-age.js
+++ jsexercism/space-age/space-age.js
@@ -6,14 +6,14 @@ const planetAPI = {
   jupiter: 11.862615,
   saturn: 29.447498,
   uranus: 84.016846,
-  neptune: 164.79132
-};
+  neptune: 164.79132,
+}
 
 const eathOrbitPer = 31557600
 
 const age = (planet, seconds) => {
-  let foo = seconds / ((eathOrbitPer)*planetAPI[planet.toLowerCase()]);
-  return parseFloat(foo.toFixed(2));
-};
-console.log(age('mercury', 2134835688));
-console.log(age('earth', 1000000000))
+  let foo = seconds / (eathOrbitPer * planetAPI[planet.toLowerCase()])
+  console.log(((foo*7).toFixed(2)))
+  return parseFloat(foo.toFixed(2)*7)
+}
+console.log(age('earth', 1000000000)) //, 221.82))
